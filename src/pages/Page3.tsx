0dcf89e44be8b749d5eb03963e5aa2348c21fd5b
lucifer_is_a_funny_name <lucifer_is_a_funny_name@gmail.com>
2025-09-06 14:40:23 +0530

    [wip] added extra questions to the fourth page

--- src/pages/Page3.tsx
+++ src/pages/Page3.tsx
@@ -2,16 +2,47 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { HeaderNavigations } from "../components/HeaderNavigations"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 // import '../styles/Page3.css'
 function Page3() {
 
-  const options = ['Answer', 'Answer', 'Answer', 'Answer']
+  // const options = ['Answer', 'Answer', 'Answer', 'Answer']
+
+  const questions = [
+    {
+      question: 'What symptoms do you have right now ?',
+      options: ["Have too much harifall", "Have minor hairfall", "Have little to no hairfall", "None of the above"]
+    },
+    {
+      question: 'What degree of damage do you have ?',
+      options: ["Thinned up hair", "Dandruff on the scalp", "Itchy scalp and irritation", "None of the above"]
+    },
+    {
+      question: 'What kind of food do you eat ?',
+      options: ["Healthy dietary food", "Outside restaurant food", "Processed or packed foods", "None of the above"]
+    },
+    {
+      question: 'How well do you exercise ?',
+      options: ["Exercise all the time", "Rarely exercise", "Exercise only on a few occasions", "None of the above"]
+    }
+  ]
 
   const navigate = useNavigate()
 
+  const [counter, setCounter] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0])
+  const [selected, setSelected] = useState("")
+
+
   const handleClick = () => {
-    navigate("/fourth")
+    if (counter === 3) {
+      navigate("/fourth")
+    } else {
+      setCurrentQuestion(questions[counter + 1])
+      setCounter(counter + 1)
+    }
+    setSelected("")
   }
 
   return (
@@ -28,11 +59,14 @@ function Page3() {
       <div className='qbox'>
         <div className='QB'>
           <div className="question">
-            1. Do you have heriditary hairfall?
+            {counter + 1}. {currentQuestion.question}
           </div>
         </div>
         <div className='options'>
-          {options.map((option) => <div key={option} className='question-options'>{option}</div>)}
+          {currentQuestion.options.map((option) => <div key={option}
+            onClick={() => {
+              setSelected(option)
+            }} className={`question-options ${selected === option ? "selected" : ""}`}>{option}</div>)}
         </div>
       </div>
       <div className='arrow' onClick={handleClick}>
